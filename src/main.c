2eac6fab0c8e03b2475c2ca38872eb7ebaf3e31b
onana <onanadonald@gmail.com>
2020-10-24 14:19:49 +0100

    not thing

--- src/main.c
+++ src/main.c
@@ -6,17 +6,19 @@
 int main(int argc, char *argv[])
 {
     Graph *graph = makeGraph("graph1.txt");
-    int i = 0; 
+    int i = 0;
+    int j = 1; 
     int n = graph->taille;
 
     for( i = 0; i < n;  i++ ) {
 
-        Element *next = graph->sommets[i]->tete;
+        Element *next = graph->sommets[0]->tete;
         printf("\n taille: %d \n", graph->sommets[i]->taille);
-        while (next != NULL)
+        while (next != NULL && j<=graph->sommets[i]->taille)
         {
             printf("\nvaleur: %d, poids: %d \n", next->valeur, next->poids);
             next = next->suivant;
+            j = j + 1;
         }
 
         printf("\n\n");
